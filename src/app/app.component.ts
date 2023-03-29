dca3bfeffb9342f659201fc942a8cd2bc6e9cf43
Arturro <arturro_1996@o2.pl>
2023-03-29 17:01:32 +0200

    finishing memes component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  emulatorType: string = "banner";
+  emulatorType: string = "memes";
 
   changeEmulatorType(type: string): void{
     this.emulatorType=type;
--- src/app/memes/memes.component.ts
+++ src/app/memes/memes.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { saveAs } from 'file-saver';
+import domtoimage from 'dom-to-image';
 
 @Component({
   selector: 'app-memes',
@@ -6,5 +8,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./memes.component.css']
 })
 export class MemesComponent {
+  memes: string[] = [
+    'img1',
+    'img2',
+    'img3',
+    'img4'
+  ]
+  className: string = 'img4';
+
+  width = 0;
+  height = 0;
+  realWidth = this.width - 250; 
+  fontSize = 45;
+
+  saveImage(element: HTMLDivElement) {
+    domtoimage.toBlob(element).then(blob => saveAs(blob));
+  }
+  getBannerStyles() {
+    return {
+      width: `500px`,
+      height: `500px`,
+      paddingLeft: `${this.width}px`,
+      paddingTop: `${this.height}px`,
+      boxSizing: `border-box`,
+    };
+  }
+  changePicture(selectedPicture: string){
+    this.className=selectedPicture;
+  }
+
 
 }
